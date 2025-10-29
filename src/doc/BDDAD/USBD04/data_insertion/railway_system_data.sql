be33f3d6e610a852568a5decd29f33bf998d7d92
Tomás Prada <1230793@isep.ipp.pt>
2025-10-29 11:29:47 +0000

    BDDAD - small update

--- src/doc/BDDAD/USBD04/data_insertion/railway_system_data.sql
+++ src/doc/BDDAD/USBD04/data_insertion/railway_system_data.sql
@@ -35,38 +35,36 @@ INSERT INTO STATION (station_id, name, type, has_warehouse, has_refrigerated, la
 
 -- 3. INSERT RAILWAY_LINE DATA (7 Lines)
 INSERT INTO RAILWAY_LINE (line_id, name, owner_operator_id, total_length_km) VALUES
-                                                                                 (1, 'Ramal São Bento - Campanhã', 'IP', 2.6),
-                                                                                 (2, 'Ramal Campanhã - Nine', 'IP', 39.0),
-                                                                                 (3, 'Ramal Nine - Barcelos', 'IP', 11.3),
-                                                                                 (4, 'Ramal Barcelos - Viana', 'IP', 30.4),
-                                                                                 (5, 'Ramal Viana - Caminha', 'IP', 24.0),
-                                                                                 (6, 'Ramal Caminha - Torre', 'IP', 20.8),
-                                                                                 (7, 'Ramal Torre - Valença', 'IP', 4.3);
+                                                                                 ('L001', 'Ramal São Bento - Campanhã', 'IP', 2.6),
+                                                                                 ('L002', 'Ramal Campanhã - Nine', 'IP', 39.0),
+                                                                                 ('L003', 'Ramal Nine - Barcelos', 'IP', 11.3),
+                                                                                 ('L004', 'Ramal Barcelos - Viana', 'IP', 30.4),
+                                                                                 ('L005', 'Ramal Viana - Caminha', 'IP', 24.0),
+                                                                                 ('L006', 'Ramal Caminha - Torre', 'IP', 20.8),
+                                                                                 ('L007', 'Ramal Torre - Valença', 'IP', 4.3);
 
 -- 4. INSERT LINE_SEGMENT DATA (13 Segments)
-INSERT INTO LINE_SEGMENT (segment_id, line_id, start_station_id, end_station_id, segment_length_km, track_type, gauge_mm, is_eletrified, max_weigth_kg_per_m, max_speed_kmh) VALUES
-                                                                                                                                                                                 (1, 1, 'ST007', 'ST005', 2.6, 'Multiple', 1668, 'Y', 8000, 80),
-                                                                                                                                                                                 (10, 2, 'ST005', 'ST020', 29.0, 'Double', 1668, 'Y', 8000, 120),
-                                                                                                                                                                                 (11, 2, 'ST020', 'ST018', 10.0, 'Double', 1668, 'Y', 8000, 120),
-                                                                                                                                                                                 (15, 3, 'ST020', 'ST008', 5.3, 'Double', 1668, 'Y', 8000, 100),
-                                                                                                                                                                                 (16, 3, 'ST008', 'ST015', 6.0, 'Double', 1668, 'Y', 8000, 100),
-                                                                                                                                                                                 (14, 4, 'ST008', 'ST017', 10.4, 'Double', 1668, 'Y', 8000, 100),
-                                                                                                                                                                                 (12, 4, 'ST017', 'ST019', 12.0, 'Double', 1668, 'Y', 8000, 100),
-                                                                                                                                                                                 (13, 4, 'ST019', 'ST012', 8.0, 'Double', 1668, 'Y', 6400, 80),
-                                                                                                                                                                                 (20, 5, 'ST017', 'ST021', 6.0, 'Double', 1668, 'Y', 8000, 100),
-                                                                                                                                                                                 (21, 5, 'ST021', 'ST022', 3.0, 'Double', 1668, 'Y', 8000, 100),
-                                                                                                                                                                                 (22, 5, 'ST022', 'ST023', 15.0, 'Double', 1668, 'Y', 8000, 100),
-                                                                                                                                                                                 (25, 6, 'ST021', 'ST016', 20.8, 'Double', 1668, 'Y', 8000, 100),
-                                                                                                                                                                                 (26, 7, 'ST016', 'ST011', 4.3, 'Double', 1668, 'Y', 8000, 100);
+INSERT INTO LINE_SEGMENT (segment_id, line_id, start_station_id, end_station_id, segment_length_km, track_type, gauge_mm, is_electrified, max_weight_kg_per_m, max_speed_kmh) VALUES
+                                                                                                                                                                                  ('SEG001', 'L001', 'ST007', 'ST005', 2.6, 'Multiple', 1668, 'Y', 8000, 80),
+                                                                                                                                                                                  ('SEG002', 'L002', 'ST005', 'ST020', 29.0, 'Double', 1668, 'Y', 8000, 120),
+                                                                                                                                                                                  ('SEG003', 'L002', 'ST020', 'ST018', 10.0, 'Double', 1668, 'Y', 8000, 120),
+                                                                                                                                                                                  ('SEG004', 'L003', 'ST020', 'ST008', 5.3, 'Double', 1668, 'Y', 8000, 100),
+                                                                                                                                                                                  ('SEG005', 'L003', 'ST008', 'ST015', 6.0, 'Double', 1668, 'Y', 8000, 100),
+                                                                                                                                                                                  ('SEG006', 'L004', 'ST008', 'ST017', 10.4, 'Double', 1668, 'Y', 8000, 100),
+                                                                                                                                                                                  ('SEG007', 'L004', 'ST017', 'ST019', 12.0, 'Double', 1668, 'Y', 8000, 100),
+                                                                                                                                                                                  ('SEG008', 'L004', 'ST019', 'ST012', 8.0, 'Double', 1668, 'Y', 6400, 80),
+                                                                                                                                                                                  ('SEG009', 'L005', 'ST017', 'ST021', 6.0, 'Double', 1668, 'Y', 8000, 100),
+                                                                                                                                                                                  ('SEG010', 'L005', 'ST021', 'ST022', 3.0, 'Double', 1668, 'Y', 8000, 100),
+                                                                                                                                                                                  ('SEG011', 'L005', 'ST022', 'ST023', 15.0, 'Double', 1668, 'Y', 8000, 100),
+                                                                                                                                                                                  ('SEG012', 'L006', 'ST021', 'ST016', 20.8, 'Double', 1668, 'Y', 8000, 100),
+                                                                                                                                                                                  ('SEG013', 'L007', 'ST016', 'ST011', 4.3, 'Double', 1668, 'Y', 8000, 100);
 
--- 5. INSERT ROLLING_STOCK DATA (4 Locomotives + 30 Wagons)
+-- 5. INSERT ROLLING_STOCK DATA (4 Locomotives + 10 Wagons)
 INSERT INTO ROLLING_STOCK (stock_id, operator_id, make, model, year_of_service, gauge_mm, length_m, width_m, height_m, tare_weight_kg, number_of_bogies) VALUES
-                                                                                                                                                             -- Locomotives
                                                                                                                                                              ('RS001', 'MEDWAY', 'Siemens', 'Eurosprinter', 1995, 1668, 19.2, 3.0, 4.375, 87000, 2),
                                                                                                                                                              ('RS002', 'MEDWAY', 'Siemens', 'Eurosprinter', 1995, 1668, 19.2, 3.0, 4.375, 87000, 2),
                                                                                                                                                              ('RS003', 'MEDWAY', 'Siemens', 'Eurosprinter', 1996, 1668, 19.2, 3.0, 4.375, 87000, 2),
                                                                                                                                                              ('RS004', 'MEDWAY', 'Sorefame - Alsthom', 'CP 1900', 1981, 1668, 19.084, 3.062, 4.31, 117000, 2),
-                                                                                                                                                             -- Wagons (primeiros 10 como exemplo - adicione os restantes)
                                                                                                                                                              ('WG001', 'MEDWAY', 'Wagon Manufacturer', 'Container Model', 2020, 1668, 14.0, 2.8, 2.9, 22000, 2),
                                                                                                                                                              ('WG002', 'MEDWAY', 'Wagon Manufacturer', 'Container Model', 2020, 1668, 14.0, 2.8, 2.9, 22000, 2),
                                                                                                                                                              ('WG003', 'MEDWAY', 'Wagon Manufacturer', 'Container Model', 2020, 1668, 14.0, 2.8, 2.9, 22000, 2),
@@ -85,7 +83,7 @@ INSERT INTO LOCOMOTIVE (stock_id, locomotive_type, power_kw, acceleration_kmh_s,
                                                                                                                                                      ('RS003', 'Electric', 5600, 0.7, 180000, NULL, 'N'),
                                                                                                                                                      ('RS004', 'Diesel', 1623, 0.4, 200000, 4882, 'N');
 
--- 7. INSERT WAGON DATA (30 Wagons)
+-- 7. INSERT WAGON DATA
 INSERT INTO WAGON (stock_id, wagon_type, payload_capacity_kg, volume_capacity_m3, container_supported, is_refrigerated, max_pressure_bar) VALUES
                                                                                                                                               ('WG001', 'Container Wagon', 60600, 76.3, '20ft,40ft,45ft', 'N', NULL),
                                                                                                                                               ('WG002', 'Container Wagon', 60600, 76.3, '20ft,40ft,45ft', 'N', NULL),
@@ -126,30 +124,5 @@ INSERT INTO BOX (box_id, qty_available, expiry_date, received_at, sku, aisle, ba
 
 COMMIT;
 
--- =============================================
--- VERIFICATION QUERIES
--- =============================================
-
--- Count records per table
-SELECT 'OPERATOR' as table_name, COUNT(*) as count FROM OPERATOR
-UNION ALL SELECT 'STATION', COUNT(*) FROM STATION
-          UNION ALL SELECT 'RAILWAY_LINE', COUNT(*) FROM RAILWAY_LINE
-          UNION ALL SELECT 'LINE_SEGMENT', COUNT(*) FROM LINE_SEGMENT
-          UNION ALL SELECT 'ROLLING_STOCK', COUNT(*) FROM ROLLING_STOCK
-          UNION ALL SELECT 'LOCOMOTIVE', COUNT(*) FROM LOCOMOTIVE
-          UNION ALL SELECT 'WAGON', COUNT(*) FROM WAGON
-          UNION ALL SELECT 'WAREHOUSE', COUNT(*) FROM WAREHOUSE
-          UNION ALL SELECT 'BAY', COUNT(*) FROM BAY
-          UNION ALL SELECT 'ITEM', COUNT(*) FROM ITEM
-          UNION ALL SELECT 'BOX', COUNT(*) FROM BOX;
-
--- Test USBD18 Query
-SELECT
-    o.name as owner_name,
-    ls.track_type,
-    COUNT(*) as segment_count
-FROM LINE_SEGMENT ls
-         JOIN RAILWAY_LINE rl ON ls.line_id = rl.line_id
-         JOIN OPERATOR o ON rl.owner_operator_id = o.operator_id
-GROUP BY o.name, ls.track_type
-ORDER BY o.name, ls.track_type;
+-- Verification
+SELECT 'USBD04 COMPLETED - Database populated successfully' as status FROM DUAL;
