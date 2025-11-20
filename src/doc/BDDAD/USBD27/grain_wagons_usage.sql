f4e0a74df5470275cdb6db3d51209a634d52d1e7
Tomás Prada <1230793@isep.ipp.pt>
2025-11-20 15:02:11 +0000

    USBD27- small updates

--- src/doc/BDDAD/USBD27/grain_wagons_usage.sql
+++ src/doc/BDDAD/USBD27/grain_wagons_usage.sql
@@ -1,11 +1,24 @@
 SELECT wagon_id
 FROM TRAIN_WAGON_USAGE
-WHERE usage_date BETWEEN DATE '2024-01-01' AND DATE '2024-12-31'
-  AND wagon_id IN (SELECT stock_id FROM WAGON WHERE wagon_type = 'Cereal Wagon')
+WHERE usage_date BETWEEN TO_DATE('&start_date', 'YYYY-MM-DD') AND TO_DATE('&end_date', 'YYYY-MM-DD')
+  AND wagon_id IN (
+    SELECT w.stock_id
+    FROM WAGON w
+             JOIN WAGON_MODEL wm ON w.model_id = wm.model_id
+    WHERE wm.wagon_type = 'Cereal Wagon'
+)
 GROUP BY wagon_id
 HAVING COUNT(DISTINCT train_id) = (
     SELECT COUNT(DISTINCT train_id)
     FROM TRAIN_WAGON_USAGE
-    WHERE usage_date BETWEEN DATE '2024-01-01' AND DATE '2024-12-31'
-      AND wagon_id IN (SELECT stock_id FROM WAGON WHERE wagon_type = 'Cereal Wagon')
+    WHERE usage_date BETWEEN TO_DATE('&start_date', 'YYYY-MM-DD') AND TO_DATE('&end_date', 'YYYY-MM-DD')
+      AND wagon_id IN (
+        SELECT w.stock_id
+        FROM WAGON w
+                 JOIN WAGON_MODEL wm ON w.model_id = wm.model_id
+        WHERE wm.wagon_type = 'Cereal Wagon'
+    )
 );
+
+# start_date : 2025-10-01
+# end_date : 2025-10-07
