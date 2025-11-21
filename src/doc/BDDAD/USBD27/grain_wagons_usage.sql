03b19b24e28df9d44be6cf3e059385c3e619206e
Tomás Prada <1230793@isep.ipp.pt>
2025-11-21 10:52:23 +0000

    USBD27 - fix query to list grain wagons used in every train that used grain wagons

--- src/doc/BDDAD/USBD27/grain_wagons_usage.sql
+++ src/doc/BDDAD/USBD27/grain_wagons_usage.sql
@@ -1,24 +1,24 @@
+-- =============================================
+-- USBD27 - Grain Wagons Used in Every Train That Used Grain Wagons
+-- =============================================
+
 SELECT wagon_id
 FROM TRAIN_WAGON_USAGE
-WHERE usage_date BETWEEN TO_DATE('&start_date', 'YYYY-MM-DD') AND TO_DATE('&end_date', 'YYYY-MM-DD')
-  AND wagon_id IN (
-    SELECT w.stock_id
-    FROM WAGON w
+WHERE usage_date BETWEEN DATE '2025-10-01' AND DATE '2025-10-07'
+  AND train_id IN (
+    SELECT DISTINCT train_id
+    FROM TRAIN_WAGON_USAGE twu
+             JOIN WAGON w ON twu.wagon_id = w.stock_id
              JOIN WAGON_MODEL wm ON w.model_id = wm.model_id
-    WHERE wm.wagon_type = 'Cereal Wagon'
+    WHERE wm.wagon_type = 'Cereal wagon'
+      AND twu.usage_date BETWEEN DATE '2025-10-01' AND DATE '2025-10-07'
 )
 GROUP BY wagon_id
 HAVING COUNT(DISTINCT train_id) = (
     SELECT COUNT(DISTINCT train_id)
-    FROM TRAIN_WAGON_USAGE
-    WHERE usage_date BETWEEN TO_DATE('&start_date', 'YYYY-MM-DD') AND TO_DATE('&end_date', 'YYYY-MM-DD')
-      AND wagon_id IN (
-        SELECT w.stock_id
-        FROM WAGON w
-                 JOIN WAGON_MODEL wm ON w.model_id = wm.model_id
-        WHERE wm.wagon_type = 'Cereal Wagon'
-    )
+    FROM TRAIN_WAGON_USAGE twu
+             JOIN WAGON w ON twu.wagon_id = w.stock_id
+             JOIN WAGON_MODEL wm ON w.model_id = wm.model_id
+    WHERE wm.wagon_type = 'Cereal wagon'
+      AND twu.usage_date BETWEEN DATE '2025-10-01' AND DATE '2025-10-07'
 );
-
--- start_date : 2025-10-01
--- end_date : 2025-10-07
