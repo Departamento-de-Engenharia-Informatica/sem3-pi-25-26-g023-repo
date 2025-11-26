0ab6b8960c6a8f3702145d23c19a35bca8fc127a
Guilherme Fernandes Gonçalves <1220887@isep.ipp.pt>
2025-11-26 16:27:13 +0000

    some changes to the function of bdda

--- src/doc/BDDAD/USBD27/grain_wagons_usage.sql
+++ src/doc/BDDAD/USBD27/grain_wagons_usage.sql
@@ -9,15 +9,7 @@ BEGIN
 OPEN v_grain_wagons FOR
 SELECT wagon_id
 FROM TRAIN_WAGON_USAGE
-WHERE usage_date BETWEEN DATE '2025-10-01' AND DATE '2025-10-07'
-  AND train_id IN (
-    SELECT DISTINCT train_id
-    FROM TRAIN_WAGON_USAGE twu
-             JOIN WAGON w ON twu.wagon_id = w.stock_id
-             JOIN WAGON_MODEL wm ON w.model_id = wm.model_id
-    WHERE wm.wagon_type = 'Cereal wagon'
-      AND twu.usage_date BETWEEN DATE '2025-10-01' AND DATE '2025-10-07'
-)
+
 GROUP BY wagon_id
 HAVING COUNT(DISTINCT train_id) = (
     SELECT COUNT(DISTINCT train_id)
